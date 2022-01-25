b959500f62afd3b0f7eb103a7cad2b1672c4b513
yanngcarvalgo <yanncarvalho@outlook.com>
2022-01-25 12:53:35 -0300

    playing with Wildfly

---
+++ app/src/main/java/br/com/yann/rssreader/factory/RequestXmlFromHttpFactory.java
@@ -0,0 +1,48 @@
+package br.com.yann.rssreader.factory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RequestXmlFromHttpFactory implements RequestXmlInterface {
+  
+    
+    //TODO implement o not found
+
+    final private OkHttpClient client;
+
+    public RequestXmlFromHttpFactory() {
+      client = new OkHttpClient().newBuilder()
+                  .connectTimeout(1, TimeUnit.MINUTES)
+                  .readTimeout(1, TimeUnit.MINUTES)
+                  .callTimeout(1, TimeUnit.MINUTES)
+                  .build();
+    }
+
+    private String xml = ""; 
+
+     public String getXml(String url) throws IOException  {
+  
+      Request request = new Request.Builder()
+                                    .url(url)
+                                    .header("Content-Type", "application/xml")
+                                   .build();
+       
+      Response  response = client.newCall(request).execute();
+      this.xml = response.body().string();  
+  
+      return  this.xml;
+      
+    }
+
+  
+
+  
+
+
+  
+}
+
---
+++ app/src/main/java/br/com/yann/rssreader/factory/RequestXmlFromLocalFileFactory.java
@@ -0,0 +1,23 @@
+package br.com.yann.rssreader.factory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class RequestXmlFromLocalFileFactory implements RequestXmlInterface {
+  private String xml = "";
+
+  @Override
+  public String getXml(String uri) throws IOException {
+
+    BufferedReader reader = new BufferedReader(new FileReader(uri));
+    String aux;
+
+    while ((aux = reader.readLine()) != null) {
+      xml = xml.concat(aux);
+    }
+
+    reader.close(); 
+    return this.xml;
+  }
+}
