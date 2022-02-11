210ddf64f2c27f09f198a1a83173780af7cd6c70
ygfcarvalho <yann.carvalho@sergipetec.org.br>
2022-02-11 12:11:12 -0300

    refact: improvement of RssController class

--- app/src/main/java/br/com/yann/rssreader/RssReaderAplication.java
+++
@@ -1,9 +0,0 @@
-package br.com.yann.rssreader;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RssReaderAplication extends Application{
-  
-}
